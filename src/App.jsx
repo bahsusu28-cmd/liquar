c5648734f88fb61a463fb974d61ec35e8e688a8f
bahsusu28-cmd <bahsusu28@gmail.com>
2026-02-28 01:37:44 -1200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Liquar - Coming Soon</h1>
+      <p>Bio link platform</p>
+    </div>
+  )
+}
+
+export default App
